dc52c49fe5fa939da1902cfb62a2cb339ef76659
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-04-29 07:14:09 +0900

    Add Corpus class.

--- lib/markovchain/corpus.rb
+++ lib/markovchain/corpus.rb
@@ -1,4 +1,16 @@
+require 'markovchain'
+
 class Markovchain
   class Corpus
+    NON_WORD = ::Markovchain::NON_WORD
+
+    attr_reader :prev_char
+
+    def initialize(gram)
+      @gram = gram
+    end
+
+    def feed_char(char)
+    end
   end
 end
--- spec/lib/markovchain/corpus_spec.rb
+++ spec/lib/markovchain/corpus_spec.rb
@@ -2,4 +2,15 @@
 require 'markovchain/corpus'
 
 describe Markovchain::Corpus do
+  describe '#prev_char' do
+    subject { corpus.prev_char }
+
+    context '1-gram' do
+      let(:corpus) { Markovchain::Corpus.new 1 }
+
+      context 'by default' do
+        it { should == Markovchain::NON_WORD }
+      end
+    end
+  end
 end
