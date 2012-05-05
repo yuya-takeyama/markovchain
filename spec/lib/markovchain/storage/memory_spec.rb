a07124b41245443df9ef8329a0b7fb7c4becddcc
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-05-05 19:08:47 +0900

    Add spec.

--- spec/lib/markovchain/storage/memory_spec.rb
+++ spec/lib/markovchain/storage/memory_spec.rb
@@ -5,17 +5,28 @@ module Markovchain::Storage
     describe '#tokens_after' do
       subject { storage.tokens_after(sequence) }
       let(:storage) { Markovchain::Storage::Memory.new }
+      let(:sequence) { 'foo' }
 
       context 'when no records matched' do
-        let(:sequence) { 'foo' }
         it { should == {} }
       end
 
       context 'when a record matched' do
         before { storage.increment(sequence, token) }
-        let(:sequence) { 'foo' }
         let(:token) { 'b' }
         it { should == {'b' => 1} }
+      end
+
+      context 'when many token seeded' do
+        before do
+          storage.increment(sequence, 'a')
+          storage.increment(sequence, 'a')
+          storage.increment(sequence, 'a')
+          storage.increment(sequence, 'b')
+          storage.increment(sequence, 'b')
+          storage.increment(sequence, 'c')
+        end
+        it { should == {'a' => 3, 'b' => 2, 'c' => 1} }
       end
     end
   end
